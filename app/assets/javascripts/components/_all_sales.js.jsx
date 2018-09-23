9ebfb9d6fac71464dc649b50d96f454dc7d8cab6
felipedf <felipe.diogenes13@gmail.com>
2018-09-23 11:28:41 -0300

    First react setup and main pages stubs created

---
+++ app/assets/javascripts/components/_all_sales.js.jsx
@@ -0,0 +1,10 @@
+class AllSales extends React.Component {
+
+  render(){
+    return(
+      <div>
+        <h1>To do: List of Sales</h1>
+      </div>
+    )
+  }
+}
---
+++ app/assets/javascripts/components/_main.js.jsx
@@ -0,0 +1,8 @@
+const Main = (props) => {
+  return (
+    <div>
+      <h1>Sales are  doing great!</h1>
+      <AllSales/>
+    </div>
+  )
+};
